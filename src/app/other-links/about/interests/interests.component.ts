9cdb2f8aa0e748c958eac6b3254ebfb207c55643
Rony <50582705+rony9707@users.noreply.github.com>
2024-07-28 03:10:34 +0530

    Play Song In Every Page

--- src/app/other-links/about/interests/interests.component.ts
+++ src/app/other-links/about/interests/interests.component.ts
@@ -1,9 +1,10 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 import { musicData } from './interests.interface';
 import { MusicControllerComponent } from './music-controller/music-controller.component';
 import { trigger, transition, style, animate, state } from '@angular/animations';
 import { BackendServiceService } from 'src/app/services/backend-service.service';
-
+import { SongService } from '../../services/song.service';
 
 @Component({
   selector: 'app-interests',
@@ -26,152 +27,80 @@ import { BackendServiceService } from 'src/app/services/backend-service.service'
       state('final', style({
         height: '100%'
       })),
-      state('initial', style({
-        height: '0%'
-      })),
       transition('initial => final', animate('.5s')),
       transition('final => initial', animate('.5s')),
     ])
   ]
 })
-export class InterestsComponent implements OnInit {
+export class InterestsComponent implements OnInit, OnDestroy {
+  constructor(private backendServer: BackendServiceService, public songService: SongService) { }
 
-  constructor(private backendServer: BackendServiceService) { }
+  @ViewChild(MusicControllerComponent) MusicControllerComponent?: MusicControllerComponent;
 
+  songProgress: number = 0;
+  animationState: string = 'initial';
+  playSongStatus = this.songService.music_status;
+  currentSongIndex: number = 0;
+
+  private updateSubscription: Subscription | undefined;
+  private progressSubscription: Subscription | undefined;
 
   ngOnInit(): void {
     this.backendServer.getSongs().subscribe(
       (mySongs) => {
-        // Extracting song links from each song object
         const songLinks = Object.values(mySongs).map(song => song.songLink);
-        // Assign the extracted song links to songName array
-        this.songNameFromBackend = songLinks;
-        this.musicData.songName=this.songNameFromBackend[0]
-
+        this.songService.setSongs(songLinks);
       }
     );
+
+    this.progressSubscription = this.songService.songProgress.subscribe(progress => {
+      this.songProgress = progress;
+    });
   }
 
-
-  @ViewChild(MusicControllerComponent) MusicControllerComponent?: MusicControllerComponent;
-
-  songProgress: number  = 0
-
-  animationState: string = 'initial';
-
-  songNameFromBackend: string[] = [];
-
-  //   songName = [
-  //   'https://dl.dropboxusercontent.com/scl/fi/9h3k1d20ipucknpxyw4b7/Ezio-s-Family.mp3?rlkey=bp5exkryh6ot6vg9wb8ou9c0m&st=g903lugy&dl=0',
-  //   'https://dl.dropbox.com/scl/fi/js7juwrseiuutzhh7ilty/Sketchbook.mp3?rlkey=higkaitf2yp7k5i9mzogcsqc4&st=p1vpanft&dl=0',
-  //   'https://dl.dropbox.com/scl/fi/s76vxgn0794imk3y870ab/Preet.mp3?rlkey=9wsj6txwvcnvhc5ugz4uwv16q&st=qajm7un5&dl=0',
-  //   'https://dl.dropbox.com/scl/fi/q6bwq2tyv5o948guiiati/Spider-Man-Into-the-Spider-Verse.mp3?rlkey=fiqhp78jjc32iqg7lzih22k49&st=lwccyv7i&dl=0',
-  //   'https://dl.dropbox.com/scl/fi/v2wzq14nvf3kn1d25et1t/Adhkhana.mp3?rlkey=p1xok43gq8y3rtaxc33nfocfp&st=fm92mzdb&dl=0'
-  // ]
-
-  // Index to keep track of the current song
-  currentSongIndex: number = 0;
-
-  //Music data to sent to music controller component
-  musicData: musicData = {
-    playSongStatus: false,
-    volumeLevel: 70,
-    songName: this.songNameFromBackend[2] //Songname is updated in ngOnInit coming from db
+  ngOnDestroy(): void {
+    if (this.updateSubscription) {
+      this.updateSubscription.unsubscribe();
+    }
+    if (this.progressSubscription) {
+      this.progressSubscription.unsubscribe();
+    }
   }
 
-  private intervalId: any;
   playSongs() {
-    this.musicData = {
-      ...this.musicData, // Spread existing properties
-      playSongStatus: !this.musicData.playSongStatus // Update playSongStatus
-    };
-
-    //Uncomment these below lines to see a good effect :3
-    // if (this.musicData.playSongStatus == true) {
-    //   console.log("song playing")
-
-
-    //   this.intervalId = setInterval(() => {
-    //     console.log('dog')
-
-    //     window.scrollTo({
-    //       top: window.scrollY - 20,
-    //       behavior: 'smooth'
-    //     });
-
-    //     setTimeout(() => {
-
-    //       window.scrollTo({
-    //         top: window.scrollY + 20,
-    //         behavior: 'smooth'
-    //       });
-
-    //     }, 500);
-
-    //   }, 1000);
-    // }
-    // else if (this.musicData.playSongStatus == false) {
-    //   clearInterval(this.intervalId);
-    //   console.log("song stopped")
-    // }
-
-  }
-
-  updateVolume(volume: number) {
-    this.musicData = {
-      ...this.musicData,
-      volumeLevel: volume
-    };
-  }
-
-  stopClickPropagation(event: MouseEvent) {
-    event.stopPropagation();
+    this.playSongStatus = !this.songService.music_status;
+    this.songService.toggleMusic(this.playSongStatus);
   }
 
   logRangeValue(event: any) {
     this.updateVolume(event.target.value);
   }
 
+  updateVolume(volume: number) {
+    this.songService.adjustVolumeSong(volume);
+  }
+
+  stopClickPropagation(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
   nextSong() {
+    this.songService.changeSong(true);
     this.animationState = 'final';
-
-
-    // Increment the current song index
-    this.currentSongIndex = (this.currentSongIndex + 1) % this.songNameFromBackend.length;
-    // Update the songName in musicData to the next song
-    this.musicData = {
-      ...this.musicData,
-      songName: this.songNameFromBackend[this.currentSongIndex]
-    };
   }
 
   resetAnimationState() {
-    this.animationState = 'initial'; // Reset animation state after it finishes
-  }
-
-  progress(value: number) {
-    this.songProgress = value
-  }
-
-  seekAudio(event: any) {
-    if (this.MusicControllerComponent) {
-      const audio = this.MusicControllerComponent.audioPlayer?.nativeElement;
-      const seekTo = (event.target.value / 100) * audio.duration;
-      audio.currentTime = seekTo;
-    }
+    this.animationState = 'initial';
   }
 
   seekToPosition(event: MouseEvent) {
-    if (this.MusicControllerComponent) {
-      const progressBar = event.target as HTMLProgressElement;
-      const seekTo = (event.offsetX / progressBar.offsetWidth) * 100;
-      if (!isNaN(seekTo)) { // Check if the click event originated from the progress bar
-        this.songProgress = seekTo;
-        this.seekAudio({ target: { value: seekTo } });
-      }
+    const progressElement = event.target as HTMLElement;
+    //const { offsetWidth, offsetLeft } = progressElement;
+    //const clickX = event.pageX - offsetLeft;
+    const seekPosition = (event.offsetX / progressElement.offsetWidth) * 100;
+    if (!isNaN(seekPosition)) { // Check if the click event originated from the progress bar
+      this.songProgress = seekPosition;
+      this.songService.seekToPosition(seekPosition);;
     }
   }
-
-
-
 }
